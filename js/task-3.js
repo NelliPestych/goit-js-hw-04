57369c466a2853e5d34d97c0a5370b2fa1b7cd59
nellipestych <NelliPestych@bigmir.net>
2024-10-27 22:02:13 +0200

    homework 4

---
+++ js/task-3.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+
+    // Метод для зміни імені профілю
+    changeUsername(newName) {
+        this.username = newName;
+    },
+
+    // Метод для оновлення часу гри
+    updatePlayTime(hours) {
+        this.playTime += hours;
+    },
+
+    // Метод для отримання інформації про профіль
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`;
+    }
+};
+
+// Перевірка функції
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
+
